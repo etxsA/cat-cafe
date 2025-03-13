194f279c62b711d40ffc3357b49413f1eab968c1
etxsA <93368439+etxsA@users.noreply.github.com>
2025-03-13 12:06:45 -0600

    Corrected Minor Mistake

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -15,7 +15,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:8000/api/login', {
+      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ usuario, password }),
